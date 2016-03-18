7d514536142e257d515c9c58f52249687da3aa3a
dylancaponi-noo <dylan.caponi@networkofone.com>
2016-03-18 16:11:58 -0700

    Update test.js

--- test.js
+++ test.js
@@ -67,12 +67,12 @@ function renderStatusX(id, statusText) {
     document.getElementById(id).innerHTML = html;
     document.getElementById(id).getElementsByTagName('p')[0].style.display = 'inline';
   }
-  //   else if (id == "author"){
-  //   var html = '<p><strong>Channel Name:&nbsp;</strong></p>' + '<br>' + statusText;
-  //   document.getElementById(id).style.margin = '16px';
-  //   document.getElementById(id).innerHTML = html;
-  //   document.getElementById(id).getElementsByTagName('p')[0].style.display = 'inline';
-  // }
+    else if (id == "author"){
+    var html = '<p><strong>Channel Name:&nbsp;</strong></p>' + '<br>' + statusText;
+    document.getElementById(id).style.margin = '16px';
+    document.getElementById(id).innerHTML = html;
+    document.getElementById(id).getElementsByTagName('p')[0].style.display = 'inline';
+  }
   else if (id == "channel_id"){
     var html = '<p><strong>Channel ID:&nbsp;</strong></p>' + '<br>' + statusText;
     document.getElementById(id).style.margin = '16px';
