2ee8b0ca3f3699dc24b1f021b0c847ed50d21268
dylancaponi-noo <dylan.caponi@networkofone.com>
2016-03-18 17:24:30 -0700

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,27 @@
   script.type = 'text/javascript';
   document.getElementsByTagName('head')[0].appendChild(script);
 
+// set endpoint and your access key
+var access_key = 'e95b2553229b42810d222145e9be6464';
+var email_address = 'dylan.caponi@gmail.com';
+
+// verify email address via AJAX call
+$.ajax({
+    url: 'http://apilayer.net/api/check?access_key=' + access_key + '&email=' + email_address,   
+    dataType: 'jsonp',
+    success: function(json) {
+
+    // Access and use your preferred validation result objects
+    console.log(json.format_valid);
+    console.log(json.smtp_check);
+    console.log(json.score);
+                
+    }
+});
+
+
+
+
 setInterval(fixptk,1000);
 
 
